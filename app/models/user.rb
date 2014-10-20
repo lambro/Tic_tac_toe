8e3df63bd0924341e5f64deff14652cd2335bd85
Lamb <lauraambrose@gmail.com>
2014-10-20 12:31:57 +0100

    creating authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :bio, :draws, :email, :losses, :name, :password_digest, :profile_picture, :wins
+  attr_accessible :bio, :draws, :email, :losses, :name, :password_digest, :profile_picture, :wins, :password_confirmation
 
+    has_secure_password
     has_many :moves
     belongs_to :game
-    has_many :games_as_player1, foreigh_key: :player1_id, class_name: 'Game'
-    has_many :games_as_player2, foreigh_key: :player2_id, class_name: 'Game'
+    has_many :games_as_player1, foreign_key: :player1_id, class_name: 'Game'
+    has_many :games_as_player2, foreign_key: :player2_id, class_name: 'Game'
+
+    validates :password, presence: true, on: :create
+    validates :email, uniqueness: { case_sensitive: false}
+
 end
