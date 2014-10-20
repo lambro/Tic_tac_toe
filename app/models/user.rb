86b924d5788b942ed02bd37e013bb8add3fd0d1e
Lamb <lauraambrose@gmail.com>
2014-10-20 17:25:25 +0100

    drop down menu sorted and basic shell done. time for rails logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ActiveRecord::Base
     validates :password, presence: true, on: :create
     validates :email, uniqueness: { case_sensitive: false}
 
+    scope :without_user, lambda { |user_id| where('users.id != ?', user_id )}
+
 end
