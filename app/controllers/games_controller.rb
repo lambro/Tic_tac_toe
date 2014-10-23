9f74e96a810f617e18331341e7660a74f4c03511
Lamb <lauraambrose@gmail.com>
2014-10-23 21:28:31 +0100

    woah.. it has broken somewhere!

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,80 +1,80 @@
 class GamesController < ApplicationController
-  # GET /games
-  # GET /games.json
-  def index
-    @games = Game.all
+# GET /games
+# GET /games.json
+def index
+  @games = Game.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @games }
-    end
+  respond_to do |format|
+  format.html # index.html.erb
+  format.json { render json: @games }
+end
+end
+
+# GET /games/1
+# GET /games/1.json
+def show
+  @game = Game.find(params[:id])
+
+  @board = @game.build_board
+
+  @result = @game.result
+  puts "@result is #{@result}"
+
+  if @result == 1
+    render action: 'result' and return
+  elsif @result == 2
+    render action: 'result' and return
+  elsif @result == 3
+    render action: 'result' and return
   end
 
-  # GET /games/1
-  # GET /games/1.json
-  def show
-    @game = Game.find(params[:id])
+  if @game.player2_id == 1 && @game.moves.present?
+    if @game.moves.last.player_id == @game.player1_id
+      computer_square_id = @game.available_square_id
 
-    @board = @game.build_board
-
-    @result = @game.result
-    puts "@result is #{@result}"
-
-    if @result == 1
-      render action: 'result', notice: "Player 1 wins" and return
-    elsif @result == 2
-      render :result,  notice: "Player 2 wins" and return
-    elsif @result == 3
-      render :result,  notice: "It's a draw" and return
-    end
-
-    if @game.player2_id == 1 && @game.moves.present?
-      if @game.moves.last.player_id == @game.player1_id
-        computer_square_id = @game.available_square_id
-
-        redirect_to new_game_move_path(@game, square_id: computer_square_id, computer: true)
-      end
-
+      redirect_to new_game_move_path(@game, square_id: computer_square_id, computer: true)
     end
 
   end
 
-  # GET /games/new
-  # GET /games/new.json
-  def new
-    @game = Game.new
+end
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @game }
+# GET /games/new
+# GET /games/new.json
+def new
+  @game = Game.new
+
+  respond_to do |format|
+  format.html # new.html.erb
+  format.json { render json: @game }
+end
+end
+
+# GET /games/1/edit
+def edit
+  @game = Game.find(params[:id])
+end
+
+# POST /games
+# POST /games.json
+def create
+  @game = Game.new(params[:game])
+
+  @game.player1_id = current_user.id
+
+  respond_to do |format|
+    if @game.save
+      format.html { redirect_to @game, notice: 'Game was successfully created.' }
+      format.json { render json: @game, status: :created, location: @game }
+    else
+      format.html { render action: "new" }
+      format.json { render json: @game.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  # GET /games/1/edit
-  def edit
-    @game = Game.find(params[:id])
-  end
-
-  # POST /games
-  # POST /games.json
-  def create
-    @game = Game.new(params[:game])
-
-    @game.player1_id = current_user.id
-
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
-        format.json { render json: @game, status: :created, location: @game }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /games/1
-  # PUT /games/1.json
+# PUT /games/1
+# PUT /games/1.json
   def update
     @game = Game.find(params[:id])
 
@@ -89,16 +89,6 @@ class GamesController < ApplicationController
     end
   end
 
-  # def move
-
-  #   @game = Game.find(params[:id])
-
-
-    
-  # end
-
-  # DELETE /games/1
-  # DELETE /games/1.json
   def destroy
     @game = Game.find(params[:id])
     @game.destroy
